b9c57822e25dabf26449e576542087e75041c3d0
PuchNik <i.p.chn.k@gmail.com>
2025-05-15 20:20:28 +0300

    Базовые понятия по AntDesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 // Глобальные импорты
+import { Col, Row } from 'antd'
 
 // Локальные импорты
+
 import './App.css'
+import { AppTable } from './components/AppTable'
+
 
 function App() {
-    return <div>Ant Design</div>
+    return (
+        <Row>
+            <Col xs={24} md={{ span: 12, offset: 6 }}>
+                <AppTable />
+            </Col>
+        </Row>
+    )
 }
 
 export default App
